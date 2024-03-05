caac7e03415fea2ab772a2fd2ac65fa629b37cf5
SynthesisDu <dupinhe@msu.edu>
2024-03-05 12:50:07 -0500

    Update newenv.sh

--- bin/dev-pyvenv/newenv.sh
+++ bin/dev-pyvenv/newenv.sh
@@ -2,6 +2,7 @@ cd ../..
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+pip install pyinstaller
 echo "Virtual environment has been created and activated."
 read -p "Press any key to continue... " -n1 -s
 echo
