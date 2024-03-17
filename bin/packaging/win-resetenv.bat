07a03e0b8fc6342871d474083ad22390ce4fd57b
SynthesisDu <dupinhe@msu.edu>
2024-03-17 00:51:49 -0400

    Update Scripts II

--- bin/packaging/win-resetenv.bat
+++ bin/packaging/win-resetenv.bat
@@ -2,6 +2,8 @@ cd /d %~dp0
 cd ../../
 for /d /r . %%d in (__pycache__) do @if exist "%%d" rd /s /q "%%d"
 for /r . %%i in (*.spec, *.toc) do @del "%%i"
+rd /s /q dist
+rd /s /q build
 docker run -it --rm -v %cd%:/docker -w /docker node:16-bullseye bash -c "npm i; npm run build; exit"
 rd /s /q /venv
 python -m venv venv
