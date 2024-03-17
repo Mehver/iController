fcf8532f2b469dd2fb286e0377c9fdcd16cc56f7
SynthesisDu <75297777+Mehver@users.noreply.github.com>
2024-03-17 00:51:49 -0400

    Update Scripts II

--- bin/packaging/mac-resetenv.sh
+++ bin/packaging/mac-resetenv.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 cd ../../
 find . -name "__pycache__" -type d -exec rm -rf {} +
 find . \( -name "*.spec" -o -name "*.toc" \) -type f -exec rm {} +
+rm -rf build dist
 docker run -it --rm -v "$(pwd)":/docker -w /docker node:16-bullseye bash -c "npm i; npm run build; exit"
 rm -rf venv
 python3 -m venv venv
