fdfc50c659bade6fab3765191e46d62f81f2dbea
Mehver | 这水怎么没味儿啊 <75297777+Mehver@users.noreply.github.com>
2024-03-19 19:56:43 -0400

    v0.3.2

--- bin/packaging/win-onefile.bat
+++ bin/packaging/win-onefile.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
 cd ../../
 call venv\Scripts\activate
-pyinstaller --noconfirm --onefile --console --icon "#README/icon/256.ico" --add-data "build;build/" --add-data "modules;modules/" "main.py"
+pyinstaller --noconfirm --onefile --console --icon "#README/icon/256a.ico" --add-data "build;build/" --add-data "modules;modules/" "main.py"
 pause
