d230b765c44c5fea4d9020eb0d38930b727f9524
SynthesisDu <75297777+Mehver@users.noreply.github.com>
2024-04-06 23:27:10 -0400

    v0.4.3

--- modules/portchecker.py
+++ modules/portchecker.py
@@ -1,20 +1,34 @@
-import psutil
-
+import socket
+import platform
 
 def portchecker(port):
     """
-    使用psutil检查端口是否可用
+    检查端口是否可用。
     - 如果端口是一个数字且在合法范围内（1-65534）
-    - 并且没有被别的程序使用，则返回True
+    - 并且没有被别的程序使用，则返回True。
     """
-    # 确保端口号是整数且在合法范围内
-    if isinstance(port, int) and 0 < port < 65535:
-        # 获取当前所有的网络连接
-        connections = psutil.net_connections()
-        # 遍历所有连接，检查端口是否被占用
-        for conn in connections:
-            if conn.laddr.port == port:
-                return False  # 端口已被占用
-        return True  # 端口未被占用，可用
+    os_name = platform.system()
+    if os_name == 'Windows':
+        # Windows下使用psutil检查
+        import psutil
+        if isinstance(port, int) and 0 < port < 65535:
+            connections = psutil.net_connections()
+            for conn in connections:
+                if conn.laddr.port == port:
+                    return False  # 端口已被占用
+            return True  # 端口未被占用，可用
+        else:
+            return False  # 端口号不合法
+    elif os_name == 'Darwin':
+        # MacOS下使用socket尝试绑定端口检查
+        if isinstance(port, int) and 0 < port < 65535:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                try:
+                    s.bind(('localhost', port))
+                    return True  # 端口未被占用，可用
+                except socket.error:
+                    return False  # 端口已被占用
+        else:
+            return False  # 端口号不合法
     else:
-        return False  # 端口号不合法
+        raise NotImplementedError("This OS is not supported for port checking")
