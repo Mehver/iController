39ff06ad0f3ec07f451d58ba45557e06ec0ae632
Mehver | 这水怎么没味儿啊 <75297777+Mehver@users.noreply.github.com>
2024-04-17 06:46:01 -0400

    Update system.py

--- modules/httpServer/system.py
+++ modules/httpServer/system.py
@@ -5,7 +5,7 @@ import platform
 async def get_system_info():
     os_name = platform.system()
     # os_name = 'Windows'  # 测试值
-    os_name = 'Darwin'  # 测试值
+    # os_name = 'Darwin'  # 测试值
     # os_version = platform.version()
     # os_arch = platform.architecture()
     # os_machine = platform.machine()
