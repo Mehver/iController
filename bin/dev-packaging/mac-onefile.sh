1364a67ec3b52c996c1e309738c1f94a2ddba15a
Mehver | 这水怎么没味儿啊 <75297777+Mehver@users.noreply.github.com>
2024-12-26 05:03:45 +0800

    Reformat Files and Update Packages

--- bin/dev-packaging/mac-onefile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-cd ../../
-source venv/bin/activate
-pyinstaller --noconfirm --onefile --console --icon "#README/icon/256a.icns" --add-data "build:build/" --add-data "modules:modules/"  "iController.py"
-if [ -z "$CI" ]; then
-  read -p "Press enter to continue"
-fi
