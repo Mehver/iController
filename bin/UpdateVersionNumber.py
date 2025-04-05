fb5fc462e65d63db5248367b0c7e6920e521a662
Mehver | 这水怎么没味儿啊 <75297777+Mehver@users.noreply.github.com>
2025-04-05 06:25:07 -0400

    Update Workflow File
    Update workflow file to support auto update version number

--- bin/UpdateVersionNumber.py
+++ bin/UpdateVersionNumber.py
@@ -1,6 +1,11 @@
-OLD_VERSION = input('Give the old version number > ')
-NEW_VERSION = input('Give the new version number > ')
+import sys
 
+if len(sys.argv) == 3:
+    OLD_VERSION = sys.argv[1]
+    NEW_VERSION = sys.argv[2]
+else:
+    OLD_VERSION = input('Give the old version number > ')
+    NEW_VERSION = input('Give the new version number > ')
 
 def UpdateVersionNumber(filename, encoder, lines_list, old_version, new_version):
     filename = "./../" + filename
@@ -13,7 +18,7 @@ def UpdateVersionNumber(filename, encoder, lines_list, old_version, new_version)
                 lines[i - 1] = line.replace(old_version, new_version)
         file.writelines(lines)
 
-
 UpdateVersionNumber('README.md', "utf-8", [3, 6], OLD_VERSION, NEW_VERSION)
 UpdateVersionNumber('docs/README-cn.md', "utf-8", [3, 6], OLD_VERSION, NEW_VERSION)
-UpdateVersionNumber('iController.py', "utf-8", [17], OLD_VERSION, NEW_VERSION)
+UpdateVersionNumber('iController.py', "utf-8", [9], OLD_VERSION, NEW_VERSION)
+UpdateVersionNumber('.github/metadata/PreviousVersion.txt', "utf-8", [1], OLD_VERSION, NEW_VERSION)
--- iController.py
+++ iController.py
@@ -6,15 +6,17 @@ from ServerApp.get_address import get_address
 from ServerApp.config import Config
 from ServerApp.pyinstaller_context import PyInstallerContext
 
-print("""
+VERSION = "v0.6.5"
+
+print(f"""
  _   ___            _             _ _           
-(_) / __\___  _ __ | |_ _ __ ___ | | | ___ _ __ 
-| |/ /  / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+(_) / __\\___  _ __ | |_ _ __ ___ | | | ___ _ __ 
+| |/ /  / _ \\| '_ \\| __| '__/ _ \\| | |/ _ \\ '__|
 | / /__| (_) | | | | |_| | | (_) | | |  __/ |   
-|_\____/\___/|_| |_|\__|_|  \___/|_|_|\___|_|   
+|_\\____/\\___/|_| |_|\\__|_|  \\___/|_|_|\\___|_|   
                                                 
 https://github.com/Mehver/iController
-v0.6.5
+{VERSION}
 
 """)
 
