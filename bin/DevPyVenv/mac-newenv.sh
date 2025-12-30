600ac5aea84335aff02a0b6f5b7ddc3c00842375
Mehver | 这水怎么没味儿啊 <75297777+Mehver@users.noreply.github.com>
2025-12-31 01:42:27 +0800

    Update DevPyVenv scripts

--- bin/DevPyVenv/mac-newenv.sh
+++ bin/DevPyVenv/mac-newenv.sh
@@ -1,7 +1,22 @@
-cd ../..
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cd ../../ || exit 1
+
+find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -type f -name "*.toc" -delete
+rm -f config.yaml
+rm -rf dist
+rm -rf build
+rm -rf venv
+rm -rf logs
+rm -rf node_modules
+rm -rf ClientBrowserUI/build
+rm -rf ClientBrowserUI/node_modules
+
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r ./requirements/iControllerGUI.txt
 echo "Virtual environment has been created and activated."
 read -p "Press any key to continue... " -n1 -s
 echo
--- bin/DevPyVenv/win-newenv.bat
+++ bin/DevPyVenv/win-newenv.bat
@@ -1,6 +1,19 @@
-cd ..\..
+cd /d %~dp0
+cd ../../
+
+for /d /r . %%d in (__pycache__) do @if exist "%%d" rd /s /q "%%d"
+for /r . %%i in (*.toc) do @del "%%i"
+del config.yaml
+rd /s /q dist
+rd /s /q build
+rd /s /q venv
+rd /s /q logs
+rd /s /q node_modules
+rd /s /q ClientBrowserUI\build
+rd /s /q ClientBrowserUI\node_modules
+
 python -m venv venv
 call venv\Scripts\activate
-pip install -r requirements.txt
-echo Virtual environment has been created and activated.
+pip install -r ./requirements/iControllerGUI.txt
+
 pause
